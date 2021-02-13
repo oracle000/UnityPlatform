f2327ccd4526359258505b625019652165c5f122
EQUINOX <johh.christopher0092@gmail.com>
2021-02-13 08:24:54 +0800

    push changes

--- Assets/Scripts/MainScript/FrogController.cs
+++ Assets/Scripts/MainScript/FrogController.cs
@@ -6,29 +6,51 @@ using UnityEngine;
 public class FrogController : MonoBehaviour
 {
 
-    private Rigidbody2D rb;    
-    private Collider2D coll;    
-    private bool moveLeft = false;
+    private Rigidbody2D _rb;    
+    private Collider2D _coll;    
+    private bool _moveLeft = true;
 
     [SerializeField] private LayerMask ground;
 
     private float speed = 0.6f;
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
-        coll = GetComponent<Collider2D>();        
+        _rb = GetComponent<Rigidbody2D>();
+        _coll = GetComponent<Collider2D>();        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(coll.IsTouchingLayers(ground))
+        if (_coll.IsTouchingLayers(ground))
         {
-            
-        } else
-        {
-
+            if (_moveLeft) 
+            {
+                transform.localScale = new Vector3(1, 1, 1);
+                _rb.velocity = new Vector2(-4, 5);
+                _moveLeft = false;
+            } else
+            {
+                transform.localScale = new Vector3(-1, 1, 1);
+                _rb.velocity = new Vector2(4, 5);
+                _moveLeft = true;
+            }
         }
+
+        //if (_moveLeft)
+        //{
+        //    if (_coll.IsTouchingLayers(ground))
+        //    {
+                
+        //    }                
+
+        //} else
+        //{
+        //    transform.localScale = new Vector3(-1, 1, 1);
+        //}
+
+
+     
         //rb.velocity = new Vector2(1, 0);
         // wayPointPos = new Vector2(wayPoint.transform.position.x, wayPoint.transform.position.y);
         // transform.position = Vector2.MoveTowards(transform.position.x, wayPointPos.x, speed * Time.deltaTime);
