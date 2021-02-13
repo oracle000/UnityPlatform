8f5e31314ca2e349798eea3eb63cf096f146c677
EQUINOX <johh.christopher0092@gmail.com>
2021-02-13 14:27:22 +0800

    add frog

--- Assets/Scripts/MainScript/FrogController.cs
+++ Assets/Scripts/MainScript/FrogController.cs
@@ -1,6 +1,4 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEditor;
 using UnityEngine;
 
 public class FrogController : MonoBehaviour
@@ -9,6 +7,8 @@ public class FrogController : MonoBehaviour
     private Rigidbody2D _rb;    
     private Collider2D _coll;    
     private bool _moveLeft = true;
+    private bool _disJump = false;
+    private bool checkFirst = true;
 
     [SerializeField] private LayerMask ground;
 
@@ -16,43 +16,39 @@ public class FrogController : MonoBehaviour
     void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
-        _coll = GetComponent<Collider2D>();        
-    }
+        _coll = GetComponent<Collider2D>();
+
+        StartCoroutine(WaitFor(Random.Range(0, 5)));
+    }    
 
     // Update is called once per frame
     void Update()
     {
-        if (_coll.IsTouchingLayers(ground))
+           
+    }
+
+
+    IEnumerator WaitFor(float value)
+    {
+        while(true)
         {
-            if (_moveLeft) 
+            yield return new WaitForSeconds(value);
+            if (_coll.IsTouchingLayers(ground))
             {
-                transform.localScale = new Vector3(1, 1, 1);
-                _rb.velocity = new Vector2(-4, 5);
-                _moveLeft = false;
-            } else
-            {
-                transform.localScale = new Vector3(-1, 1, 1);
-                _rb.velocity = new Vector2(4, 5);
-                _moveLeft = true;
+                if (_moveLeft)
+                {
+                    transform.localScale = new Vector3(1, 1, 1);
+                    _rb.velocity = new Vector2(-4, 5);
+                    _moveLeft = false;
+                }
+                else
+                {
+                    transform.localScale = new Vector3(-1, 1, 1);
+                    _rb.velocity = new Vector2(4, 5);
+                    _moveLeft = true;
+                }
             }
         }
-
-        //if (_moveLeft)
-        //{
-        //    if (_coll.IsTouchingLayers(ground))
-        //    {
-                
-        //    }                
-
-        //} else
-        //{
-        //    transform.localScale = new Vector3(-1, 1, 1);
-        //}
-
-
-     
-        //rb.velocity = new Vector2(1, 0);
-        // wayPointPos = new Vector2(wayPoint.transform.position.x, wayPoint.transform.position.y);
-        // transform.position = Vector2.MoveTowards(transform.position.x, wayPointPos.x, speed * Time.deltaTime);
+                         
     }
 }
